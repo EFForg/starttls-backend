53d90e5d9323f5552b5509ab0e11db1745c838e0
sydneyli <sydney@eff.org>
2018-04-24 11:07:41 -0700

    Cleanup on main

--- main.go
+++ main.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-    "errors"
     "log"
     "net/http"
-    "os"
 
     "github.com/sydneyli/starttls-scanner/db"
 )
-
-// Default configuration values. Can be overwritten by env vars of the same name.
-var configDefaults = map[string]string {
-    "PORT"            : ":8080",
-    "DB_NAME"         : "starttls",
-    "DB_USERNAME"     : "postgres",
-    "DB_PASSWORD"     : "postgres",
-    "DB_TOKEN_TABLE"  : "tokens",
-    "DB_DOMAIN_TABLE" : "domains",
-    "DB_SCAN_TABLE"   : "scans",
-}
-
-func getEnvOrDefault(var_name string) string {
-    env_var := os.Getenv(var_name)
-    if len(env_var) == 0 {
-        env_var = configDefaults[var_name]
-    }
-    return env_var
-}
 
 // Serves all public HTTPS endpoints.
 func ServePublicEndpoints() {
