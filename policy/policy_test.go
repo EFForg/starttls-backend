d1116d1aa76cef8ce393ef0cf30aaa49691e9b10
sydneyli <sydney@eff.org>
2018-06-17 20:53:33 -0700

    test(policy list): configure update timer

--- policy/policy_test.go
+++ policy/policy_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
+	"time"
 )
 
 var mockList = list{
@@ -21,7 +22,7 @@ func mockErroringFetchHTTP() (list, error) {
 }
 
 func TestGetPolicy(t *testing.T) {
-	list := makeUpdatedList(mockFetchHTTP)
+	list := makeUpdatedList(mockFetchHTTP, time.Hour)
 
 	policy, err := list.Get("not-on-the-list.com")
 	if err == nil {
@@ -38,9 +39,28 @@ func TestGetPolicy(t *testing.T) {
 }
 
 func TestFailedListUpdate(t *testing.T) {
-	list := makeUpdatedList(mockErroringFetchHTTP)
+	list := makeUpdatedList(mockErroringFetchHTTP, time.Hour)
 	_, err := list.Get("eff.org")
 	if err == nil {
 		t.Errorf("Get should return an error if fetching the list fails")
+	}
+}
+
+func TestListUpdate(t *testing.T) {
+	var updatedList = list{Policies: map[string]TLSPolicy{}}
+	list := makeUpdatedList(func() (list, error) { return updatedList, nil }, time.Second)
+	_, err := list.Get("example.com")
+	if err == nil {
+		t.Error("Getting the policy for an unlisted domain should return an error")
+	}
+	// Update the list!
+	updatedList.Policies["example.com"] = TLSPolicy{Mode: "testing"}
+	time.Sleep(time.Second * 2)
+	policy, err := list.Get("example.com")
+	if err != nil {
+		t.Errorf("Unexpected error while getting policy: %s", err)
+	}
+	if !reflect.DeepEqual(policy, updatedList.Policies["example.com"]) {
+		t.Errorf("Expected policy for example.com to be %v, got %v", mockList.Policies["eff.org"], policy)
 	}
 }
