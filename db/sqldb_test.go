aa16f06e3843b7ca277c036cf12092c333291875
vivian <vivian@eff.org>
2018-07-13 13:08:31 -0700

    Fix Travis database connection

--- db/sqldb_test.go
+++ db/sqldb_test.go
@@ -1,7 +1,6 @@
 package db_test
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"testing"
@@ -19,7 +18,7 @@ func initTestDb() *db.SQLDatabase {
 	os.Setenv("PRIV_KEY", "./certs/key.pem")
 	os.Setenv("PUBLIC_KEY", "./certs/cert.pem")
 	cfg, err := db.LoadEnvironmentVariables()
-	cfg.DbName = fmt.Sprintf("%s_dev", cfg.DbName)
+	// cfg.DbName = fmt.Sprintf("%s_dev", cfg.DbName)
 	if err != nil {
 		log.Fatal(err)
 	}
