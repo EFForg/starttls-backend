65d0c7595771d025025b2ddcf1bb655ae00c3635
sydneyli <sydney@eff.org>
2018-09-05 15:12:57 -0700

    chore: lint and fix policy_test

--- policy/policy_test.go
+++ policy/policy_test.go
@@ -7,29 +7,29 @@ import (
 	"time"
 )
 
-var mockList = list{
+var mockList = List{
 	Policies: map[string]TLSPolicy{
 		"eff.org": TLSPolicy{Mode: "testing"},
 	},
 }
 
-func mockFetchHTTP() (list, error) {
+func mockFetchHTTP() (List, error) {
 	return mockList, nil
 }
 
-func mockErroringFetchHTTP() (list, error) {
-	return list{}, fmt.Errorf("something went wrong")
+func mockErroringFetchHTTP() (List, error) {
+	return List{}, fmt.Errorf("something went wrong")
 }
 
 func TestGetPolicy(t *testing.T) {
-	list := makeUpdatedList(mockFetchHTTP, time.Hour)
+	List := makeUpdatedList(mockFetchHTTP, time.Hour)
 
-	policy, err := list.Get("not-on-the-list.com")
+	policy, err := List.Get("not-on-the-List.com")
 	if err == nil {
-		t.Error("Getting the policy for an unlisted domain should return an error")
+		t.Error("Getting the policy for an unListed domain should return an error")
 	}
 
-	policy, err = list.Get("eff.org")
+	policy, err = List.Get("eff.org")
 	if err != nil {
 		t.Errorf("Unexpected error while getting policy: %s", err)
 	}
@@ -39,24 +39,24 @@ func TestGetPolicy(t *testing.T) {
 }
 
 func TestFailedListUpdate(t *testing.T) {
-	list := makeUpdatedList(mockErroringFetchHTTP, time.Hour)
-	_, err := list.Get("eff.org")
+	List := makeUpdatedList(mockErroringFetchHTTP, time.Hour)
+	_, err := List.Get("eff.org")
 	if err == nil {
-		t.Errorf("Get should return an error if fetching the list fails")
+		t.Errorf("Get should return an error if fetching the List fails")
 	}
 }
 
 func TestListUpdate(t *testing.T) {
-	var updatedList = list{Policies: map[string]TLSPolicy{}}
-	list := makeUpdatedList(func() (list, error) { return updatedList, nil }, time.Second)
-	_, err := list.Get("example.com")
+	var updatedList = List{Policies: map[string]TLSPolicy{}}
+	List := makeUpdatedList(func() (List, error) { return updatedList, nil }, time.Second)
+	_, err := List.Get("example.com")
 	if err == nil {
-		t.Error("Getting the policy for an unlisted domain should return an error")
+		t.Error("Getting the policy for an unListed domain should return an error")
 	}
-	// Update the list!
+	// Update the List!
 	updatedList.Policies["example.com"] = TLSPolicy{Mode: "testing"}
 	time.Sleep(time.Second * 2)
-	policy, err := list.Get("example.com")
+	policy, err := List.Get("example.com")
 	if err != nil {
 		t.Errorf("Unexpected error while getting policy: %s", err)
 	}
@@ -66,12 +66,12 @@ func TestListUpdate(t *testing.T) {
 }
 
 func TestDomainsToValidate(t *testing.T) {
-	var updatedList = list{Policies: map[string]TLSPolicy{
+	var updatedList = List{Policies: map[string]TLSPolicy{
 		"eff.org":     TLSPolicy{},
 		"example.com": TLSPolicy{},
 	}}
-	list := makeUpdatedList(func() (list, error) { return updatedList, nil }, time.Second)
-	domains, _ := list.DomainsToValidate()
+	List := makeUpdatedList(func() (List, error) { return updatedList, nil }, time.Second)
+	domains, _ := List.DomainsToValidate()
 	if !reflect.DeepEqual([]string{"eff.org", "example.com"}, domains) {
 		t.Errorf("Expected eff.org and example.com to be returned")
 	}
@@ -79,10 +79,10 @@ func TestDomainsToValidate(t *testing.T) {
 
 func TestHostnamesForDomain(t *testing.T) {
 	hostnames := []string{"a", "b", "c"}
-	var updatedList = list{Policies: map[string]TLSPolicy{
+	var updatedList = List{Policies: map[string]TLSPolicy{
 		"eff.org": TLSPolicy{MXs: hostnames}}}
-	list := makeUpdatedList(func() (list, error) { return updatedList, nil }, time.Second)
-	returned, err := list.HostnamesForDomain("eff.org")
+	List := makeUpdatedList(func() (List, error) { return updatedList, nil }, time.Second)
+	returned, err := List.HostnamesForDomain("eff.org")
 	if err != nil {
 		t.Fatalf("Encountered %v", err)
 	}
