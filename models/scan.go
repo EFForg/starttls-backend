86379cf6b25a412f6ae5ac3e24980c0346259947
Vivian Brown <vivian@eff.org>
2019-01-16 17:36:53 -0800

    Lint

--- models/scan.go
+++ models/scan.go
@@ -8,12 +8,14 @@ import (
 	"github.com/EFForg/starttls-backend/checker"
 )
 
+// Scan stores the result of a scan of a domain
 type Scan struct {
 	Domain    string               `json:"domain"`    // Input domain
 	Data      checker.DomainResult `json:"scandata"`  // Scan results from starttls-checker
 	Timestamp time.Time            `json:"timestamp"` // Time at which this scan was conducted
 }
 
+// WriteHTML renders a scan result as HTML.
 func (s Scan) WriteHTML(w http.ResponseWriter) error {
 	tmpl := template.Must(template.ParseFiles("views/scan.html.tmpl"))
 	return tmpl.Execute(w, s)
