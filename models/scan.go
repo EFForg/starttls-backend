1e186b56a1b9ef193b37f7d31dfad958a9c653dd
sydneyli <sydney@eff.org>
2019-01-22 14:56:37 -0800

    Add versioning to scan caching

--- models/scan.go
+++ models/scan.go
@@ -6,9 +6,12 @@ import (
 	"github.com/EFForg/starttls-backend/checker"
 )
 
+const CurrentVersion = 1
+
 // Scan stores the result of a scan of a domain
 type Scan struct {
 	Domain    string               `json:"domain"`    // Input domain
 	Data      checker.DomainResult `json:"scandata"`  // Scan results from starttls-checker
 	Timestamp time.Time            `json:"timestamp"` // Time at which this scan was conducted
+	Version   uint32               `json:"version"`   // Version counter
 }
