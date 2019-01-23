00b784d91de08125215dfe8c12e0d0ae6c256a2d
Vivian Brown <vivian@eff.org>
2019-01-23 11:42:35 -0800

    Render domain status to HTML

--- api_test.go
+++ api_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/EFForg/starttls-backend/checker"
@@ -39,4 +43,22 @@ func TestPolicyCheckWithQueuedDomain(t *testing.T) {
 	if result.Status != checker.Warning {
 		t.Errorf("Check should have warned.")
 	}
+}
+
+func testHTMLPost(path string, data url.Values, t *testing.T) ([]byte, int) {
+	req, err := http.NewRequest("POST", server.URL+path, strings.NewReader(data.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("accept", "text/html")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	if !strings.Contains(strings.ToLower(string(body)), "<html") {
+		t.Errorf("Response should be HTML, got %s", string(body))
+	}
+	return body, resp.StatusCode
 }
