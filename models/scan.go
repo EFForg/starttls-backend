389827b3635cd6b4e204bdbc39a78e558a7808b2
Vivian Brown <vivian@eff.org>
2019-01-22 17:58:39 -0800

    Let controller choose template

--- models/scan.go
+++ models/scan.go
@@ -1,9 +1,6 @@
 package models
 
 import (
-	"html/template"
-	"io"
-	"os"
 	"time"
 
 	"github.com/EFForg/starttls-backend/checker"
@@ -14,20 +11,4 @@ type Scan struct {
 	Domain    string               `json:"domain"`    // Input domain
 	Data      checker.DomainResult `json:"scandata"`  // Scan results from starttls-checker
 	Timestamp time.Time            `json:"timestamp"` // Time at which this scan was conducted
-}
-
-// WriteHTML renders a scan result as HTML.
-func (s Scan) WriteHTML(w io.Writer) error {
-	data := struct {
-		Scan
-		BaseURL string
-	}{
-		Scan:    s,
-		BaseURL: os.Getenv("FRONTEND_WEBSITE_LINK"),
-	}
-	tmpl, err := template.ParseFiles("views/scan.html.tmpl")
-	if err != nil {
-		return err
-	}
-	return tmpl.Execute(w, data)
 }
