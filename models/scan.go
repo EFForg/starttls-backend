cd629a86a902edabb2e6435902d76c639c9b3c53
sydneyli <sydney@eff.org>
2019-01-22 17:27:40 -0800

    lint/comment

--- models/scan.go
+++ models/scan.go
@@ -6,6 +6,7 @@ import (
 	"github.com/EFForg/starttls-backend/checker"
 )
 
+// ScanVersion is the version of the Scan API that the binary is currently using.
 const ScanVersion = 1
 
 // Scan stores the result of a scan of a domain
